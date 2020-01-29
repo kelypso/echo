277d31a35b7813e18837f381df57de3dcf806df3
kelypso <kklowlow@gmail.com>
2020-01-28 19:27:07 -0700

    update flight seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,21 @@ DatabaseCleaner.clean_with(:truncation)
 roamer = User.create!(name: "Conrad", email: "roamer@email.com", username: "roamer", password: "roamer")
 scout = User.create!(name: "Lacey", email: "scout@email.com", username: "scout", password: "scout")
 
+flight_a = Flight.create!(aircraft: "C152", aircraft_id: "5443L", 
+                        departure: "OXR", arrival: "SZP", classification: "single engine land",
+                        position: "dual", duration: "1.4", conditions: "day",
+                        ground: "0.5", landings: "1 day", 
+                        remarks: "Climbs, turns, cleaning trim, stalls, steep turns, slow flight",                        
+                        user_id: "1")
+flight_b = Flight.create!(aircraft: "C150M", aircraft_id: "714HH", 
+                        departure: "SZP", arrival: "SZN", classification: "single engine land",
+                        position: "dual", duration: "1.2", conditions: "day",
+                        ground: "0.4", landings: "1 day", 
+                        remarks: "Cross wind to landing, s-turns",
+                        user_id: "2")
+flight_c = Flight.create!(aircraft: "C150M", aircraft_id: "704UT", 
+                        departure: "SZP", arrival: "SBA", classification: "single engine land",
+                        position: "dual", duration: "1.7", conditions: "day",
+                        ground: "0.3", landings: "4 day", 
+                        remarks: "Steep turns, slow flight, descent high low, stall, SP traffic pattern, take off/land",
+                        user_id: "2")

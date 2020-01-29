7397b3263e28ed8a45506ebf53cf7beadf9bad98
kelypso <kklowlow@gmail.com>
2020-01-29 14:29:29 -0700

    begin building redux store; install react-redux, react-router-dom, redux, and thunk

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        Welcome to ECHO
       </div>
     )
   }

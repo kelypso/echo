d9ac2b3e525a13977f9b6cfd1b5397b237f894d9
kelypso <kklowlow@gmail.com>
2020-01-28 19:25:20 -0700

    add flight serializer

---
+++ app/serializers/flight_serializer.rb
@@ -0,0 +1,4 @@
+class FlightSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :date, :aircraft, :aircraft_id, :departure, :arrival, :classification, :position, :duration, :conditions, :ground, :landings, :remarks
+end

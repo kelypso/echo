3c5dabe2579262e74c046f5625ae416ca5944d11
kelypso <kklowlow@gmail.com>
2020-01-28 19:13:17 -0700

    add model relationships and validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :flights, dependent: :destroy
+
+    validates :name, :email, :username, presence: true
+    validates :email, :username, uniqueness: true
 end

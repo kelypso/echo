0bad8c1b4b72ff1f3fa453ce148dfb182415fe06
kelypso <kklowlow@gmail.com>
2020-01-29 15:34:27 -0700

    import login action to login comp

---
+++ client/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {login} from '../actions/user'
+//import {updateLoginForm} from '../actions/loginForm'
+
+const Login = ({loginData, updateLoginForm, login, history}) => {
+    const {username, password} = loginData
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        updateLoginForm(name, value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        login(loginData, history)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="username" onChange={handleChange} value={username} placeholder="username" className="formInput" /><br/><br/>
+            <input type="password" name="password" onChange={handleChange} value={password} placeholder="password" className="formInput" /><br />
+            <br/><input type="submit" value="Login" className="formBtn" />
+        </form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        loginData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, {updateLoginForm, login})(Login)

545ddc08b2bd7271627fc63b308f8032d0793a5b
kelypso <kklowlow@gmail.com>
2020-01-29 17:00:17 -0700

    update login, logout, signup routes; change get_current_user to find in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  post "/api/v1/login", to: "api/v1/sessions#create"
-  post "/api/v1/signup", to: "api/v1/users#create"
-  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
-  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
-
   namespace :api do 
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: [:index, :create]
       resources :flights
+      get '/:username', to: 'users#show', as: :user
+      get '/find_user', to: 'sessions#find'
+      post '/login', to: 'sessions#create'
+      post '/signup', to: 'users#create'
+      delete '/logout', to: 'sessions#destroy'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

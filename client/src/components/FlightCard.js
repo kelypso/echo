02220fd035913aab6132f29f08d54d7c1c8cd956
kelypso <kklowlow@gmail.com>
2020-01-30 22:41:44 -0700

    remove active class name from flight card

--- client/src/components/FlightCard.js
+++ client/src/components/FlightCard.js
@@ -5,10 +5,10 @@ const FlightCard = ({flight}) => {
     return (
         flight ?
             <div className="FlightCard">
-                <h3><Link to={`/flights/${flight.id}`} className="card-link card-date" activeClassName="selected" key={flight.id}>{flight.attributes.date}</Link></h3>
+                <h3><Link to={`/flights/${flight.id}`} className="card-link card-date" key={flight.id}>{flight.attributes.date}</Link></h3>
                 <p className="card-details">{flight.attributes.departure} - {flight.attributes.arrival}</p>
                 <p className="card-details">{flight.attributes.aircraft} / {flight.attributes.aircraft_id}</p>
-                <Link to={`/flights/${flight.id}/edit`} className="card-link card-edit" activeClassName="selected">Edit</Link>
+                <Link to={`/flights/${flight.id}/edit`} className="card-link card-edit">Edit</Link>
             </div> :
         <p>Flight could not be found.</p>
     )

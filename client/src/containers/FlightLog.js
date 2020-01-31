7d36f533fe7e877f82f032332b8e26384096cda7
kelypso <kklowlow@gmail.com>
2020-01-30 22:54:11 -0700

    update flight log imports

--- client/src/containers/FlightLog.js
+++ client/src/containers/FlightLog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import FlightCard from './FlightCard'
+import FlightCard from '../components/FlightCard'
 
 const FlightLog = ({flights}) => {
     const flightLog = flights.length > 0 ? flights.map(f => <FlightCard flight={f} key={f.id} />) : null
